7294b09c27e26dbf73da8f963baefe0cc7c1cb7c
ghassen abdelaziz <ghassenabdelaziz2023@gmail.com>
2024-12-27 21:06:33 +0100

    code

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { QuestionComponent } from './question/question.component';
+import { HeaderComponent } from './header/header.component';
+import { PagenameComponent } from './pagename/pagename.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ChangeBgDirective } from './change-bg.directive';
+import { CoursComponent } from './cours/cours.component';
+import { InterviewComponent } from './interview/interview.component';
+import { DocComponent } from './doc/doc.component';
+import { WelcQuizComponent } from './welc-quiz/welc-quiz.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    QuestionComponent,
+    HeaderComponent,
+    PagenameComponent,
+    ChangeBgDirective,
+    CoursComponent,
+    InterviewComponent,
+    DocComponent,
+    WelcQuizComponent,
+    FooterComponent,
+
+ 
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/cours/cours.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cours',
+  templateUrl: './cours.component.html',
+  styleUrls: ['./cours.component.css']
+})
+export class CoursComponent {
+  searchText: any;
+
+  names = [
+      { titre: 'angular'},
+      { titre: 'react'},
+      { titre: 'nest js'},
+    
+  ];
+}
